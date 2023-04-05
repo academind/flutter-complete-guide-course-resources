a4f3d23dcacdc43a9c8fb5037634c5069751b06d
Maximilian Schwarzmüller <28806196+maxschwarzmueller@users.noreply.github.com>
2023-04-05 08:47:51 +0200

    updated starting code of section 2

--- Code Snapshots/02 Flutter & Dart Basics I/01 Starting Setup/lib/main.dart
+++ Code Snapshots/02 Flutter & Dart Basics I/01 Starting Setup/lib/main.dart
@@ -1,1 +1,37 @@
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('First App'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const [
+              Text(
+                'Hello World!',
+              ),
+              Text(
+                'It\'s time to learn Flutter!',
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
