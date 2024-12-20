e0fc5c34d3d22320ddec69f512edce68114728c8
Maximilian Schwarzmüller <28806196+maxschwarzmueller@users.noreply.github.com>
2024-12-20 07:28:21 +0100

    updated index.js

--- Lecture Attachments/14 Chat App/index.js
+++ Lecture Attachments/14 Chat App/index.js
@@ -1,24 +1,20 @@
-const functions = require('firebase-functions');
-const admin = require('firebase-admin');
-
+const functions = require("firebase-functions/v2/firestore");
+const admin = require("firebase-admin");
+ 
 admin.initializeApp();
-
-// Cloud Firestore triggers ref: https://firebase.google.com/docs/functions/firestore-events
-exports.myFunction = functions.firestore
-  .document('chat/{messageId}')
-  .onCreate((snapshot, context) => {
-    // Return this function's promise, so this ensures the firebase function
-    // will keep running, until the notification is scheduled.
+exports.myFunction = functions.onDocumentCreated(
+  "chat/{messageId}",
+  (event) => {
+    const data = event.data.data();
     return admin.messaging().send({
-      // Sending a notification message.
       notification: {
-        title: snapshot.data()['username'],
-        body: snapshot.data()['text'],
+        title: data["username"],
+        body: data["text"],
       },
       data: {
-        // Data payload to be sent to the device.
-        click_action: 'FLUTTER_NOTIFICATION_CLICK',
+        click_action: "FLUTTER_NOTIFICATION_CLICK",
       },
-      topic: 'chat',
+      topic: "chat",
     });
-  });
+  }
+);
